2c431b399c7af803f9f7a857a6c39ac4c9ff930a
Ben Crinion <ben.crinion@gmail.com>
2018-02-17 21:07:17 +0000

    Fixed path for view folder to work on hosting environment

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ var app = express()
 app.engine('mustache', mustacheExpress());
 
 app.set('view engine', 'mustache');
-app.set('views', __dirname + '\\views');
+app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
     res.render('index', {
